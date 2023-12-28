124a31a61694c0cba20061aac1d56ef5a30a3702
buzz <chobuzz23@gmail.com>
2023-12-28 23:19:50 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import styles from "./App.module.css"
+import Navbar from "./components/Navbar/Navbar"
+
+function App() {
+
+  // 15:54
+
+  return (
+    <div className={styles.App}>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
